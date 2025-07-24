a33fe9225b8d2d7037c97a719adfae3d2989afb9
akl-githu <mohamedakl082@gmail.com>
2025-07-24 10:32:29 +0400

    creating login.css file for styling login page

--- obj/Debug/net8.0/controlCenter.AssemblyInfo.cs
+++ obj/Debug/net8.0/controlCenter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("controlCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4a6dfcda507dc2c3cbf77d2d86072904cb4d92c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59928aec5b6ca1ad49911c5293d1a338494d061b")]
 [assembly: System.Reflection.AssemblyProductAttribute("controlCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("controlCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
